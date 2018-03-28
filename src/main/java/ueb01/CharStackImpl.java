bd4e2bc6c4a86a4aa3a87f0da8c9f944d4264bb3
sWIFlohitz <lhitzlinger@web.de>
2018-03-28 16:41:36 +0200

    .

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,39 @@
 package ueb01;
 
+import java.util.NoSuchElementException;
+
 class CharStackImpl implements CharStack {
+    class Element {
+        char value;
+        Element next;
+        Element (char value, Element next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+    int counter;
+    Element head;
+    @Override
+    public void push(char c) {
+        counter++;
+        Element it = head;
+        head = new Element(c,it);
+
+    }
+
+    @Override
+    public char pop() {
+        if (head==null){
+            throw new NoSuchElementException();
+        }
+        counter--;
+        char c = head.value;
+        head=head.next;
+        return c;
+    }
+
+    @Override
+    public int size() {
+        return counter;
+    }
 }
